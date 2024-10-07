e5cae7d8474cd7dc7a01863a7ac57183c89ba607
Ilhomjon <72074602+IlhomBek-F@users.noreply.github.com>
2024-10-07 11:41:59 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,13 @@
-// import React, { FC, useState } from "react";
-// import "../src/App.css";
-// function App() {
-//   const [value, setValue] = useState<string>("");
-
-//   const [todoStore, setTodoStore] = useState<ListTodo[]>([]);
-//   type formEvent = React.FormEvent<HTMLFormElement>;
-
-//   interface ListTodo {
-//     item: string;
-//     done: boolean;
-//   }
-//   const onSubmitHandle = (e: formEvent) => {
-//     e.preventDefault();
-//     newTodoAdd(value);
-//   };
-
-//   const newTodoAdd = (todoItem: string) => {
-//     const addNewTodo: ListTodo[] = [
-//       { item: todoItem, done: false },
-//       ...todoStore,
-//     ];
-
-//     setTodoStore(addNewTodo);
-//   };
-
-//   const handleComp = (index: number) => {
-//     const newTodoItem: ListTodo[] = [...todoStore];
-//     newTodoItem[index].done = !newTodoItem[index].done;
-
-//     setTodoStore(newTodoItem);
-//   };
-
-//   const removeTodoItem = (index: number) => {
-//     const remainTodo: ListTodo[] = [...todoStore];
-//     remainTodo.splice(index, 1);
-
-//     setTodoStore(remainTodo);
-//   };
-//   return (
-//     <div className="App">
-//       <h2>Todo List</h2>
-//       <form onSubmit={onSubmitHandle}>
-//         <input
-//           type="text"
-//           placeholder="todo"
-//           value={value}
-//           onChange={(e) => setValue(e.target.value)}
-//         />
-//         <button>Add</button>
-//       </form>
-//       <ul>
-//         {todoStore.map((itm: ListTodo, index: number) => {
-//           return (
-//             <li>
-//               {itm.item}
-//               <button onClick={() => handleComp(index)}>
-//                 {itm.done ? "incomplete" : "complete"}
-//               </button>
-//               <button onClick={() => removeTodoItem(index)}>remove</button>;
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { FC, useState, useRef, useEffect } from "react";
 import "./App.css";
+
+ type Todo = {
+    name: string;
+    id: number
+  }
+
 function App() {
   const [value, setValue] = useState<string>("");
-
   const [todoStore, setTodoStore] = useState<Todo[]>([]);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -81,21 +17,15 @@ function App() {
     }
   }, []);
 
-  interface Todo {
-    item: string;
-  }
-
-  const addNewTodo = (item: string) => {
-    const newTodo: Todo[] = [{ item: item }, ...todoStore];
+  const addNewTodo = (name: string) => {
+    const newTodo: Todo[] = [{ name, id: todoStore.length + 1}, ...todoStore];
 
     setTodoStore(newTodo);
     setValue("");
-    console.log(newTodo);
   };
 
-  const deleteItem = (index: number) => {
-    const remainItem: Todo[] = [...todoStore];
-    remainItem.splice(index, 1);
+  const deleteItem = (id: number) => {
+    const remainItem: Todo[] = todoStore.filter((todo) => todo.id !== id);
 
     setTodoStore(remainItem);
   };
@@ -112,10 +42,10 @@ function App() {
         <button onClick={() => addNewTodo(value)}>Add</button>
       </div>
       <ul className="ul">
-        {todoStore.map((item: Todo, index: number) => {
+        {todoStore.map((todo: Todo, index: number) => {
           return (
-            <li className="item">
-              {item.item}
+            <li className="item" key={todo.id}>
+              {todo.name}
               <button onClick={() => deleteItem(index)} className="delete">
                 x
               </button>
